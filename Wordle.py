9662e119006018a790acd7fdff6e87b7dd74c207
nathandanderson <nathand.anderson@outlook.com>
2022-10-05 12:02:28 -0600

    Some more changes

--- Wordle.py
+++ Wordle.py
@@ -5,10 +5,12 @@ This module is the starter file for the Wordle assignment.
 BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 """
 
+from ast import And
+from dataclasses import MISSING
 import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import CORRECT_COLOR, WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import CORRECT_COLOR, MISSING_COLOR, PRESENT_COLOR, WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
 
@@ -29,12 +31,21 @@ def wordle():
             current_letter = gw.get_square_letter(gw.get_current_row(),x)
             enteredWordList.append(current_letter.lower())
 
-            # Compare positions in entered list with wordle list to see if letters are in the correct position then color green
-            if current_letter.lower() == wordleWordList[x]:
-                gw.set_square_color(gw.get_current_row(),x,CORRECT_COLOR)
-                gw.set_key_color(current_letter, CORRECT_COLOR)
+        # THIS DOES THE COLORS FOR THE WORDLE BY LOOPING THROUGH A SERIES OF IF STATEMENTS TO ASSIGN THE CORRECT COLOR.
+            for y in enteredWordList:
+                if current_letter.lower() == wordleWordList[x]:
+                    gw.set_square_color(gw.get_current_row(),x,CORRECT_COLOR)
+                    gw.set_key_color(current_letter, CORRECT_COLOR)
+                elif current_letter.lower() in wordleWordList:
+                    gw.set_square_color(gw.get_current_row(),x,PRESENT_COLOR)
+                    gw.set_key_color(current_letter, PRESENT_COLOR)
+                else:
+                    gw.set_square_color(gw.get_current_row(),x,MISSING_COLOR)
+                    gw.set_key_color(current_letter, MISSING_COLOR)
+
             # Increment the column position.
             x=x+1
+
         # Form a word based on the inputed word list for searching through the dictionary
         for z in enteredWordList:
             enteredWord += z
@@ -42,6 +53,7 @@ def wordle():
         # Search through the dictionary and confirm that the word entered is in the dictionary
         if enteredWord not in FIVE_LETTER_WORDS:
             gw.show_message("Please enter a valid word.")
+
         # If the word is in the dictionary then move on to the next row
         else:
             # CHANGE THE ROW TO THE NEXT ROW
