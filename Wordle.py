4c39338d10a12a8a1f34918b631a4af8051f0ecc
nathandanderson <nathand.anderson@outlook.com>
2022-10-05 11:27:00 -0600

    things should work now

--- Wordle.py
+++ Wordle.py
@@ -8,12 +8,13 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 import random
 
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import CORRECT_COLOR, WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
 
 # CHOOSE THE WORDLE WORD
     wordleWord = FIVE_LETTER_WORDS[random.randint(0,len(FIVE_LETTER_WORDS)-1)]
+    wordleWordList = list(wordleWord)
     print(wordleWord)
 
 
@@ -23,6 +24,8 @@ def wordle():
         enteredWord = ''
         while x <= (N_COLS-1):
             enteredWordList.append(gw.get_square_letter(gw.get_current_row(),x).lower())
+            if gw.get_square_letter(gw.get_current_row(),x).lower() == wordleWordList[x]:
+                gw.set_square_color(gw.get_current_row(),x,CORRECT_COLOR)
             x=x+1
         for z in enteredWordList:
             enteredWord += z
