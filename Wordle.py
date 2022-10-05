664d816da6289a029947a56abc6b2e839609673d
nathandanderson <nathand.anderson@outlook.com>
2022-10-05 11:07:45 -0600

    Some Changes

--- Wordle.py
+++ Wordle.py
@@ -12,8 +12,28 @@ from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
 
 def wordle():
 
+# CHOOSE THE WORDLE WORD
+    wordleWord = FIVE_LETTER_WORDS[random.randint(0,len(FIVE_LETTER_WORDS)-1)]
+    print(wordleWord)
+
+
     def enter_action(s):
-        gw.show_message("You have to implement this method.")
+        x = 0
+        enteredWordList = list()
+        enteredWord = ''
+        while x <= (N_COLS-1):
+            enteredWordList.append(gw.get_square_letter(gw.get_current_row(),x).lower())
+            x=x+1
+        for z in enteredWordList:
+            enteredWord += z
+        if enteredWord not in FIVE_LETTER_WORDS:
+            gw.show_message("Please enter a valid word.")
+        else:
+            # CHANGE THE ROW TO THE NEXT ROW
+            if gw.get_current_row()+1 < N_ROWS:
+                gw.set_current_row(gw.get_current_row()+1)
+
+            gw.show_message("Nice word choice! Guess again!")
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
